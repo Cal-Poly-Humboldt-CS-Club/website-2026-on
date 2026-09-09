c7863ea605bb52d138800f8d099a1730eff779e0
Andrew <contact@andw.dev>
2026-09-08 19:50:18 -0700

    Updated join page styles

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,55 +1,74 @@
 import styles from "./page_join.module.css";
 import Button from "@/components/Button";
+import Image from "next/image";
 
 export default function JoinPage() {
   return (
-    <div className={styles.page}>
-      <h1>Join the Club!</h1>
+    <div className={styles.bodyContent}>
+      <div className={styles.topBackground}>
+        <Image
+          src="/background3.svg"
+          alt="selection of icons"
+          width="3517"
+          height="750"
+          loading="eager"
+        />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" aria-hidden="true" preserveAspectRatio="none">
+          <path fillOpacity="1" d="M0,262 Q 720,204 1440,262 L1440,320 L0,320 Z"></path>
+        </svg>
+      </div>
 
-      <section className={styles.section}>
-        <h2>Who can join?</h2>
-        <p>
-          We welcome all Cal Poly Humboldt students, regardless of major or experience level.
-          Whether you're new to Computer Science, already have a career in CS, or just curious there is a place for you here.
-        </p>
-        <br />
-        <p>
-          Not a student at Cal Poly Humboldt? You are still welcome to join meetings and discussions regardless!
-        </p>
-      </section>
+      <div className={styles.titleContent}>
+        <h1>Join the Club!</h1>
+        <p>Meet, learn, and build with a welcoming community of students who love technology.</p>
+      </div>
 
-      <section className={styles.section}>
-        <h2>When & Where We Meet</h2>
-        <p>
-          We typically meet every two weeks on Mondays from <b>5-6:30pm</b> in BSS <b>BSS 313</b> (Fall 2026).
-        </p>
-        {/* <p>
-          For the fall semester, this will be on 8/27, 9/10, 9/24, and 10/8, 10/22 before we do the ICPC competition, and then 11/5, 11/19, and 12/3 afterwards.
-        </p> */}
-        <p>
-          In the spring semester, we will continue our bi-weekly meetings on campus. Will announce meeting dates.
-        </p>
-      </section>
+      <div className={styles.twoWideContainer}>
+        <section className={styles.section}>
+          <h2>Who can join?</h2>
+          <p>
+            We welcome all Cal Poly Humboldt students, regardless of major or experience level.
+            Whether you're new to Computer Science, already have a career in CS, or just curious there is a place for you here.
+          </p>
+          <p>
+            Not a student at Cal Poly Humboldt? You are still welcome to join meetings and discussions regardless!
+          </p>
+        </section>
 
-      <section className={styles.section}>
-        <h2>How to Get Involved</h2>
-        <ul>
-          <li>Attend meetings</li>
-          <li>Join our Discord community</li>
-          <li>Participate in projects and events</li>
-        </ul>
-      </section>
+        <section className={styles.section}>
+          <h2>When &amp; Where We Meet</h2>
+          <p>
+            We typically meet every two weeks on Mondays from <b>5-6:30pm</b> in BSS <b>BSS 313</b> (Fall 2026).
+          </p>
+          <p>
+            In the spring semester, we will continue our bi-weekly meetings on campus. Will announce meeting dates.
+          </p>
+        </section>
+      </div>
 
-      <section className={styles.section}>
-        <h2>Discord</h2>
-        <p>
-          Join our Discord to stay updated, ask questions, and connect with other members.
-        </p>
+      <div className={styles.lowerSectionWrap}>
+        <section className={styles.lowerFeatureCard}>
+          <div className={styles.cardLabel}>Get involved</div>
+          <h2>How to Get Involved</h2>
+          <ul>
+            <li>Attend meetings</li>
+            <li>Join our Discord community</li>
+            <li>Participate in projects and events</li>
+          </ul>
+        </section>
 
-        <Button className={styles.discordButton} href="/discord">
-          Join Discord
-        </Button>
-      </section>
+        <section className={styles.lowerFeatureCardAccent}>
+          <div className={styles.cardLabel}>Community</div>
+          <h2>Discord</h2>
+          <p>
+            Join our Discord to stay updated, ask questions, and connect with other members.
+          </p>
+
+          <Button className={styles.discordButton} href="/discord">
+            Join Discord
+          </Button>
+        </section>
+      </div>
     </div>
   );
 }
